95b7a60eba0aa4b4382e89f0f237b35fa0e2144d
Ramanan S <rams2@publicisgroupe.net>
2021-12-20 23:56:22 +0530

    strapi backend setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7241262a101b2a50c9a239e431489b97'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0ccf95e-7d04-4d8a-a314-cbf888fb2739'
+};
